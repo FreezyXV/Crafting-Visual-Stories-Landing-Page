a6566c36d1836a3c097acc57cbd3d930c482e57a
FreezyXV <i_841@mail.ru>
2025-02-10 12:40:53 +0100

    Fix CraftingVision import issue

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Crafting-Visual-Stories-Landing-Page/" : "/",
-  outputDir: "dist"
+  publicPath: "/Crafting-Visual-Stories-Landing-Page/",
 });
